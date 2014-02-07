a27d19b2763e6080a9757e6ed214eabff13194f7
Tyler <tylercampbell@mac.com>
2014-02-06 22:11:56 -0800

    Basic Timer Panel

--- src/com/tyler/SmiteTimers/panels/TimerPanel.java
+++ src/com/tyler/SmiteTimers/panels/TimerPanel.java
@@ -1,5 +1,11 @@
 package com.tyler.SmiteTimers.panels;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -7,27 +13,37 @@ import javax.swing.SpringLayout;
 
 public class TimerPanel extends JPanel {
 
-    public TimerPanel() {
+    JLabel timer;
+    JLabel title;
+
+    public TimerPanel(long time, String titleText, String imagePath) {
 
         SpringLayout layout = new SpringLayout();
         this.setLayout(layout);
 
-        JLabel label = new JLabel("Label: ");
-        this.add(label);
-        JTextField textField = new JTextField("Text field", 15);
-        this.add(textField);
+        // Create the Title layout
+        this.title = new JLabel(titleText);
+        this.add(this.title);
+        layout.putConstraint(SpringLayout.WEST, this.title, 5, SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.NORTH, this.title, 5, SpringLayout.NORTH, this);
 
-layout.putConstraint(SpringLayout.WEST, label,
-                     5,
-                     SpringLayout.WEST, this);
-layout.putConstraint(SpringLayout.NORTH, label,
-                     5,
-                     SpringLayout.NORTH, this);
-layout.putConstraint(SpringLayout.WEST, textField,
-                     5,
-                     SpringLayout.EAST, label);
-layout.putConstraint(SpringLayout.NORTH, textField,
-                     5,
-                     SpringLayout.NORTH, this);        
+        // Create timer layout
+        this.timer = new JLabel("00:00");
+        this.add(this.timer);
+        layout.putConstraint(SpringLayout.NORTH, this.timer, 5, SpringLayout.SOUTH, title); // Above timer
+        layout.putConstraint(SpringLayout.WEST, this.timer, 5, SpringLayout.WEST, this);        
+
+        // Add in an image icon
+        if(imagePath != null) {
+            try {
+                BufferedImage myPicture = ImageIO.read(new File("path-to-file"));
+                JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+                this.add(picLabel);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
